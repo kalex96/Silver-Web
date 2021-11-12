212b906ed43da79ccf1bc506513c2b04ef05b803
j0s34m1g0 <68119912+j0s34m1g0@users.noreply.github.com>
2021-11-12 13:54:42 -0500

    se añade un campo al esquema de usuarios

--- esquema/usuarios.js
+++ esquema/usuarios.js
@@ -5,7 +5,8 @@ const {Schema} = mongoose;
 const UsuariosSchema = new Schema({
     correo: {type:String, required:true},
     nombre: {type:String, required:true}, 
-    cargo: {type:String, required:true}
+    cargo: {type:String, required:true},
+    addTo: {type:String, required:true}
 });
 
 module.exports = mongoose.model('Usuarios', UsuariosSchema)
