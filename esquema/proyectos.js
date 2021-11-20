45198ccd6f25a729e6dc21ce2cbc09ba758b2c93
j0s34m1g0 <68119912+j0s34m1g0@users.noreply.github.com>
2021-11-20 13:37:26 -0500

    se modifican los endpoints de usuarios y proyectos

--- esquema/proyectos.js
+++ esquema/proyectos.js
@@ -1,36 +1,33 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const AvanceSchema = new Schema({
-    idProyecto: {type:String, required:true},
-    fecha: {type:Date, required:true},
-    descripcion: {type:String, required:true},
-    observacion: {type:String, required:true},
-    idEstudiante: {type:String, required:true},
-    idLider: {type:String, required:true}
-});
-
-const InscripcionesSchema = new Schema({
-    idProyecto: {type:String, required:true},
-    idEstudiante: {type:String, required:true},
-    estado: {type:String, required:true},
-    fechaIngreso: {type:Date, required:true},
-    fechaEgreso: {type:Date, required:true}
-});
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const ProyectosSchema = new Schema({
-    nombre: {type:String, required:true},
-    oGenerales: {type:String, required:true},
-    oEspecificos: {type:String, required:true},
-    presupuesto: {type:String, required:true},
-    fechaInicio: {type:Date, required:true},
-    fechaFin: {type:Date, required:true},
-    idLider: {type:String, required:true},
-    estado: {type:String, required:true},
-    fase: {type:String, required:true},
-    estudiantes: {type:Array, required:true}, 
-    avance: [AvanceSchema],
-    inscripciones:[InscripcionesSchema] 
+  nombre: { type: String, required: true },
+  oGenerales: { type: String, required: true },
+  oEspecificos: { type: String, required: true },
+  presupuesto: { type: String, required: true },
+  fechaInicio: { type: Date, required: true },
+  fechaFin: { type: Date, required: true },
+  idLider: { type: String, required: true },
+  estado: { type: String, required: true },
+  fase: { type: String, required: true },
+  avance: [
+    {
+      fecha: { type: Date, required: true },
+      descripcion: { type: String, required: true },
+      observacion: { type: String, required: true },
+      idEstudiante: { type: String, required: true },
+      idLider: { type: String, required: true }
+    },
+  ],
+  inscripciones: [
+    {
+      idEstudiante: { type: String, required: true },
+      estado: { type: String, required: true },
+      fechaIngreso: { type: Date, required: true },
+      fechaEgreso: { type: Date, required: true }
+    },
+  ],
 });
 
-module.exports = mongoose.model('Proyectos', ProyectosSchema)
+module.exports = mongoose.model("Proyectos", ProyectosSchema);
--- rutas/proyectos.js
+++ rutas/proyectos.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 const Proyectos = require("../esquema/proyectos");
+const Inscripciones = Proyectos.Inscripciones;
 
 router.get("/", async (req, res) => {
   const proyectos = await Proyectos.find();
@@ -21,7 +21,7 @@ router.post("/", async (req, res) => {
     fase,
     estudiantes,
     avance,
-    inscripciones
+    inscripciones,
   } = req.body;
   const proyectos = new Proyectos({
     nombre,
@@ -35,7 +35,7 @@ router.post("/", async (req, res) => {
     fase,
     estudiantes,
     avance,
-    inscripciones
+    inscripciones,
   });
   await proyectos.save();
   res.json({ status: "Proyecto guardado" });
@@ -54,7 +54,7 @@ router.put("/:id", async (req, res) => {
     fase,
     estudiantes,
     avance,
-    inscripciones
+    inscripciones,
   } = req.body;
   const nuevoProyecto = {
     nombre,
@@ -68,7 +68,40 @@ router.put("/:id", async (req, res) => {
     fase,
     estudiantes,
     avance,
-    inscripciones
+    inscripciones,
+  };
+  await Proyectos.findByIdAndUpdate(req.params.id, nuevoProyecto);
+  res.json({ status: "Proyecto actualizado" });
+});
+
+router.put("/:id", async (req, res) => {
+  const {
+    nombre,
+    oGenerales,
+    oEspecificos,
+    presupuesto,
+    fechaInicio,
+    fechaFin,
+    idLider,
+    estado,
+    fase,
+    estudiantes,
+    avance,
+    inscripciones,
+  } = req.body;
+  const nuevoProyecto = {
+    nombre,
+    oGenerales,
+    oEspecificos,
+    presupuesto,
+    fechaInicio,
+    fechaFin,
+    idLider,
+    estado,
+    fase,
+    estudiantes,
+    avance,
+    inscripciones,
   };
   await Proyectos.findByIdAndUpdate(req.params.id, nuevoProyecto);
   res.json({ status: "Proyecto actualizado" });
@@ -82,6 +115,81 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const proyectoxId = await Proyectos.findById(req.params.id);
   res.json(proyectoxId);
+});
+
+router.post("/agregar-inscripcion", async (req, res) => {
+  if (req.body._id) {
+    Proyectos.updateOne(
+      { _id: req.body._id },
+      {
+        $push: {
+          inscripciones: {
+            idEstudiante: req.body.idEstudiante,
+            estado: req.body.estado,
+            fechaIngreso: req.body.fechaIngreso,
+            fechaEgreso: req.body.fechaEgreso,
+          },
+        },
+      },
+      (error) => {
+        if (error) {
+          return res.json({
+            success: false,
+            msj: "No se pudo agregar la inscripcion",
+            err,
+          });
+        } else {
+          return res.json({
+            success: true,
+            msj: "Se agregó correctamente la inscripcion",
+          });
+        }
+      }
+    );
+  } else {
+    return res.json({
+      success: false,
+      msj: "No se pudo agregar la inscripcion, por favor verifique que el _id del proyecto sea correcto",
+    });
+  }
+});
+
+router.post("/agregar-avance", async (req, res) => {
+  if (req.body._id) {
+    Proyectos.updateOne(
+      { _id: req.body._id },
+      {
+        $push: {
+          avance: {
+            fecha: req.body.fecha,
+            descripcion: req.body.descripcion,
+            observacion: req.body.observacion,
+            idEstudiante: req.body.idEstudiante,
+            idLider: req.body.idLider,
+          },
+        },
+      },
+      (error) => {
+        if (error) {
+          return res.json({
+            success: false,
+            msj: "No se pudo agregar el avance",
+            err,
+          });
+        } else {
+          return res.json({
+            success: true,
+            msj: "Se agregó correctamente el avance",
+          });
+        }
+      }
+    );
+  } else {
+    return res.json({
+      success: false,
+      msj: "No se pudo agregar el avance, por favor verifique que el _id del proyecto sea correcto",
+    });
+  }
 });
 
 module.exports = router;
